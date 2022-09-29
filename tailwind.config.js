ffe84c073e385dfa616876685e0395e01bd16591
ryn-crypto <riyanfirst13@gmail.com>
2022-09-29 22:10:27 +0700

    menggunakan tailwind untuk membuat protfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#0f172a',
+      }
+    },
+  },
+  plugins: [],
+}
