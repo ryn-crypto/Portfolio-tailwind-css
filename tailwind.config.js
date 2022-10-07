deb94a7f9632ca53482c3b3a77faa7d225443a07
ryn-crypto <riyanfirst13@gmail.com>
2022-10-07 19:19:03 +0700

    memperbaiki tata letak element hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      paddikng:'160x',
+    },
     extend: {
       colors: {
         primary: '#14b8a6',
         dark: '#0f172a',
-      }
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
     },
   },
   plugins: [],
